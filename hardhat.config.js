e83216be58e3e8fb7a1d5fef3a0a8db991eb70c3
solenemep <solene.pettier@gmail.com>
2022-11-25 11:46:21 -0300

    ok

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+require('@nomicfoundation/hardhat-toolbox');
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-solhint');
+require('hardhat-docgen');
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: '0.8.17',
+};
