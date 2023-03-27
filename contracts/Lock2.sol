d793833b618de7ac2be3c7814828744dee5d0ed4
solenemep <solene.pettier@gmail.com>
2023-03-27 10:05:04 -0300

    creation lock2 + test push

---
+++ contracts/Lock2.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+import "./Registry.sol";
+
+contract Lock2 is OwnableUpgradeable {
+    address public treasury;
+
+    mapping(address => uint256) internal _lockedAmounts;
+
+    function __Lock_init() external initializer {
+        __Ownable_init();
+    }
+
+    function setDependencies(address registryAddress) external onlyOwner {
+        // treasury = Registry(registryAddress).getContract("TREASURY");
+    }
+
+    function lock(uint256 amount) external {
+        _lockedAmounts[msg.sender] += amount;
+    }
+
+    function add(uint256 a) external pure returns (uint256) {
+        return a + 10;
+    }
+}
